6ab74145d63a57e082958ad1e345ff4246902694
Dxtobi <44758414+Dxtobi@users.noreply.github.com>
2022-09-07 17:24:46 +0100

    fixing  issuse

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ if (process.env.NODE_ENV === 'production') {
   // Set static folder
   //app.use(express.static('client/build'));
   app.use('*', express.static(path.join(__dirname, "client", "build")))
- /* app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });*/
+  app.get('*', (req, res) => {
+    res.sendFile(express.static(path.resolve(__dirname, 'client', 'build', 'index.html')));
+  });
 }
 
 app.use('/api/students', require('./routes/students'));
