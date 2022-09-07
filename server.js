3b932202cc22245e0b295a0aa529d4c99d50b471
Dxtobi <44758414+Dxtobi@users.noreply.github.com>
2022-09-07 17:41:30 +0100

    fixing  issuse

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.use(require('helmet')());
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   //app.use(express.static('client/build'));
-  app.use('*', express.static(path.join(__dirname, "client", "build")))
- /* app.get('*', (req, res) => {
+  app.use(express.static(path.join(__dirname, "client", "build")))
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });*/
+  });
 }
 
 app.use('/api/students', require('./routes/students'));
