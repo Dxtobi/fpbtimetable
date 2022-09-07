d76e816588e337924cda7d495553aea0b8fc5799
Dxtobi <44758414+Dxtobi@users.noreply.github.com>
2022-09-07 23:48:46 +0100

    not giving up on this code ;)

--- server.js
+++ server.js
@@ -24,12 +24,20 @@ if(process.env.NODE_ENV === 'production') {
 
   */
 
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+
+  /*
   app.use(express.static("client/build"));
  // app.use(express.static(path.join(__dirname, 'client', 'build')));
    app.get("*", (req, res) => {
       res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
    });
-
+*/
 }
 
 app.use(require('helmet')());
