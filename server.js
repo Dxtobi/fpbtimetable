8419e461badbde430a41cbba8c1ccba4a94a4cf3
Dxtobi <44758414+Dxtobi@users.noreply.github.com>
2022-09-07 18:41:15 +0100

    fixing  issuse

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const path = require('path');
 require('./models/db');
 const app = express();
 app.use(require('cors')());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(require('helmet')());
+app.use('/api/students', require('./routes/students'));
+app.use('/api/courses', require('./routes/course'));
 
 
 // Production
@@ -23,11 +29,5 @@ if(process.env.NODE.ENV === 'production') {
 }
 const PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(require('helmet')());
-app.use('/api/students', require('./routes/students'));
-app.use('/api/courses', require('./routes/course'));
 
 app.listen(PORT, () => console.log(`App running on port ${PORT}`)  );
