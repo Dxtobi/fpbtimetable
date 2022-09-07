874b226bb9f5f155661117984be6629b147eda92
Dxtobi <44758414+Dxtobi@users.noreply.github.com>
2022-09-07 21:56:43 +0100

    fixing  issuse

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 
 
+app.use(require('helmet')());
+app.use('/api/students', require('./routes/students'));
+app.use('/api/courses', require('./routes/course'));
 
 // Production
 if(process.env.NODE.ENV === 'production') {
@@ -26,9 +29,6 @@ if(process.env.NODE.ENV === 'production') {
 
 }
 
-app.use(require('helmet')());
-app.use('/api/students', require('./routes/students'));
-app.use('/api/courses', require('./routes/course'));
 const PORT = process.env.PORT || 5000;
 
 
