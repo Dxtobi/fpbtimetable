fc7d51d01ab458f110143e646de079f592e3ea84
Dxtobi <44758414+Dxtobi@users.noreply.github.com>
2022-09-07 23:32:50 +0100

    not giving up on this code ;)

--- server.js
+++ server.js
@@ -9,17 +9,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 
 
-app.use(require('helmet')());
-app.use('/api/students', require('./routes/students'));
-app.use('/api/courses', require('./routes/course'));
-
 // Production
 if(process.env.NODE_ENV === 'production') {
-  /*app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-  });*/
+  /*
+
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+    app.get('*', (req, res) => {
+
+      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+
+    });
+
+  */
 
   app.use(express.static("client/build"));
  // app.use(express.static(path.join(__dirname, 'client', 'build')));
@@ -28,6 +31,11 @@ if(process.env.NODE_ENV === 'production') {
    });
 
 }
+
+app.use(require('helmet')());
+app.use('/api/students', require('./routes/students'));
+app.use('/api/courses', require('./routes/course'));
+
 
 const PORT = process.env.PORT || 5000;
 
