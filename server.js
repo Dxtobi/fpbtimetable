c01acfb9f6a977aff0db76131876e0e1bf5eeca1
Dxtobi <44758414+Dxtobi@users.noreply.github.com>
2022-09-08 02:31:31 +0100

    not giving up on this code ;)

--- server.js
+++ server.js
@@ -8,21 +8,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-
+app.use(require('helmet')());
+app.use('/api/students', require('./routes/students'));
+app.use('/api/courses', require('./routes/course'));
 // Production
 if(process.env.NODE_ENV === 'production') {
-
-  /*
-
-    app.use(express.static(path.join(__dirname, 'client', 'build')));
-
-    app.get('*', (req, res) => {
-
-      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-
-    });
-
-  */
 
   // Serve any static files
   app.use(express.static(path.join(__dirname, "client/build")));
@@ -31,6 +21,7 @@ if(process.env.NODE_ENV === 'production') {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 
+  
   /*
   app.use(express.static("client/build"));
  // app.use(express.static(path.join(__dirname, 'client', 'build')));
