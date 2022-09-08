888186b353fc3781f2d36ea1159f029252c82d55
Dxtobi <44758414+Dxtobi@users.noreply.github.com>
2022-09-08 04:50:58 +0100

    not giving up on this code ;)

--- server.js
+++ server.js
@@ -7,10 +7,16 @@ app.use(require('cors')());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
+if(process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static('client/build'));
+}
+app.get('*',(req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 // Production
-if(process.env.NODE_ENV === 'production') {
+/*if(process.env.NODE_ENV === 'production') {
   //app.use(express.static('client/build'));
 
    // Serve any static files
@@ -22,7 +28,7 @@ if(process.env.NODE_ENV === 'production') {
 }
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, "/client/build", "index.html"));
-});
+});*/
 app.use(require('helmet')());
 app.use('/api/students', require('./routes/students'));
 app.use('/api/courses', require('./routes/course'));
