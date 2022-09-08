d24309815e26948ea73902ffc72cd8a1f385749e
Dxtobi <44758414+Dxtobi@users.noreply.github.com>
2022-09-08 04:30:23 +0100

    not giving up on this code ;)

--- server.js
+++ server.js
@@ -11,20 +11,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Production
 if(process.env.NODE_ENV === 'production') {
-
+  app.use(express.static('client/build'));
  try {
    // Serve any static files
-   app.use(express.static(path.join(__dirname, "/client/build")));
+  // app.use(express.static(path.join(__dirname, "/client/build")));
    // Handle React routing, return all requests to React app
-   app.get('/', function (req, res) {
-     res.sendFile(path.join(__dirname, "/client/build", "index.html"));
-   });
+   
  } catch (error) {
   console.log(error.message, "from server")
  }
 
 }
-
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+});
 app.use(require('helmet')());
 app.use('/api/students', require('./routes/students'));
 app.use('/api/courses', require('./routes/course'));
